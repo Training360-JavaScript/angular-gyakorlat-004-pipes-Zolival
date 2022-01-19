379c965ae285fd411538384b7ac756bc891b744d
Zolival <zolinak@gmail.com>
2022-01-19 01:29:12 +0100

    test is ok

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -13,14 +13,14 @@ export class SorterPipe implements PipeTransform {
    */
   transform(value: any[], key: string): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
-
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb vagy nincs megadva a key,
      * térj vissza a value változóval.
      */
-
+    if(!Array.isArray(value) || !key){
+      return value;
+    }
 
 
     /**
@@ -32,6 +32,18 @@ export class SorterPipe implements PipeTransform {
      * 3. Térj vissza a két string localeCompare metódus által visszaadott
      *  összehasonlításának az eredményével.
      */
+
+  return value.sort( (a, b) => {
+    if(typeof a[key] == "number" && typeof b[key] == "number" ){
+      return a[key] - b[key]
+    }
+
+    if(typeof a[key] != "number" && typeof b[key] != "number" ){
+      return a[key].toString().toLowerCase()
+      .localeCompare(
+        b[key].toString().toLowerCase() )
+    }
+  });
 
 
   }
